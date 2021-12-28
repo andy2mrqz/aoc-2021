1d4ce2680739974af8ddef732be2a62a683bcce8
Andrew Marquez <andrew.marquez@taxbit.com>
2021-12-28 02:14:33 -0600

    Solve day 1 part 2

--- src/advent_of_code/day01.clj
+++ src/advent_of_code/day01.clj
@@ -8,7 +8,9 @@
        (s/split-lines)
        (map #(Integer/parseInt %))))
 
-(defn -part1 [nums]
+(defn -part1
+  "Solution I came up with myself!"
+  [nums]
   (loop [xs (rest nums) prev (first nums) count 0]
     (if (empty? xs)
       count
@@ -17,3 +19,23 @@
         (recur (rest xs) (first xs) count)))))
 
 (-part1 input)
+;; 1451
+
+(defn -part2
+  "Inspired by the cleaner solution to part 1, but I did it all myself!"
+  [nums]
+  (->> (partition 2 1 (partition 3 1 nums))
+       (filter (fn [[x y]] (> (reduce + y) (reduce + x))))
+       count))
+
+(-part2 input)
+
+;; (comment
+;;   (defn -part1
+;;     "Solution I found after that I like:
+;;      https://gist.github.com/Solaxun/a84b956dc885657dc23e15f826495133"
+;;     [nums]
+;;     (->> (partition 2 1 nums)
+;;          (filter (fn [[x y]] (> y x)))
+;;          count))
+;;   (-part1 input))
