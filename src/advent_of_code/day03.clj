14b72db57b97a47f4cd26d1a773851f6337c2115
Andrew Marquez <andrew.marquez@taxbit.com>
2022-01-02 19:55:41 -0600

    Solve day 3 part 2

--- src/advent_of_code/day03.clj
+++ src/advent_of_code/day03.clj
@@ -9,24 +9,40 @@
 (defn transpose [v] (apply mapv vector v))
 
 (defn find-bits-by
-  [find-min-or-max-by]
+  [gt-or-lt]
   (fn [bits-at-position]
-    (->> (frequencies bits-at-position)
-         (apply find-min-or-max-by val)
-         (key))))
-
-(defn flip-bits [binary-string] (s/join (map #(if (= \0 %) "1" "0") binary-string)))
+    (as-> (frequencies bits-at-position) freqs
+      (case (compare (freqs \0) (freqs \1))
+        1  (if (= > gt-or-lt) \0 \1)
+        0  (if (= > gt-or-lt) \1 \0)
+        -1 (if (= > gt-or-lt) \1 \0)))))
 
 (defn binary->decimal [binary-string] (Integer/parseInt binary-string 2))
 
 (defn -part1
-  [binary-numbers]
-  (let [transposed (transpose binary-numbers)
-        gamma-rate  (s/join (map (find-bits-by max-key) transposed))
-        epsilon-rate (flip-bits gamma-rate)
-        gamma-int (binary->decimal gamma-rate)
-        epsilon-int (binary->decimal epsilon-rate)]
-    (* gamma-int epsilon-int)))
+  [nums]
+  (let [transposed (transpose nums)
+        gamma-rate  (s/join (map (find-bits-by >) transposed))
+        epsilon-rate (s/join (map (find-bits-by <) transposed))]
+    (reduce * (map binary->decimal [gamma-rate epsilon-rate]))))
 
 (-part1 input)
 ;; => 1307354
+
+(defn find-rating
+  [gt-or-lt]
+  (fn [remaining pos]
+    (let [most-or-least-common ((find-bits-by gt-or-lt) (nth (transpose remaining) pos))
+          bits-starting-with (filter #(= most-or-least-common (nth % pos)) remaining)]
+      (if (= 1 (count bits-starting-with))
+        (reduced bits-starting-with)
+        bits-starting-with))))
+
+(defn -part2
+  [nums]
+  (let [[o2-rating] (reduce (find-rating >) nums (range (count (first nums))))
+        [co2-rating] (reduce (find-rating <) nums (range (count (first nums))))]
+    (reduce * (map binary->decimal [o2-rating co2-rating]))))
+
+(-part2 input)
+;; => 482500
