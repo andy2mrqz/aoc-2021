7f779d1e20602e9453a973ce761c16c0df7ba327
Andrew Marquez <andrew.marquez@taxbit.com>
2022-01-06 14:37:30 -0700

    Solve day 5 part 2

--- src/advent_of_code/day05.clj
+++ src/advent_of_code/day05.clj
@@ -7,7 +7,7 @@
 (def coordinates
   (->> (slurp "resources/day05.txt")
        (s/split-lines)
-       (mapv #((comp parse-nums rest first re-seq) #"(\d+),(\d+) -> (\d+),(\d+)" %))))
+       (mapv #((comp parse-nums re-seq) #"\d+" %))))
 
 (defn x-or-y-match? [[x0 y0 x1 y1]] (or (= x0 x1) (= y0 y1)))
 
@@ -18,14 +18,22 @@
     (for [y (get-range y0 y1)] [x0 y])
     (for [x (get-range x0 x1)] [x y0])))
 
-(defn determine-overlaps [coll]
-  (->> (group-by identity coll)
-       (filterv #(>= ((comp count val) %) 2))))
+(defn into-lines-pt-2 [[x0 y0 x1 y1 :as input]]
+  (if (x-or-y-match? input)
+    (into-lines input)
+    (let [x-range (if (> x1 x0) (range x0 (inc x1)) (range x0 (dec x1) -1))
+          y-range (if (> y1 y0) (range y0 (inc y1)) (range y0 (dec y1) -1))]
+      (map vector x-range y-range))))
 
-(defn -part1 [input]
-  (->> input
-       (filterv x-or-y-match?)
-       ((comp count determine-overlaps mapcat) into-lines)))
+(defn determine-overlaps [coll] (->> (frequencies coll) (filterv #(>= (val %) 2))))
+
+(defn solve [into-line-fn input] ((comp count determine-overlaps mapcat) into-line-fn input))
+
+(defn -part1 [input] (->> (filterv x-or-y-match? input) (solve into-lines)))
+(defn -part2 [input] (solve into-lines-pt-2 input))
 
 (-part1 coordinates)
 ;; => 5147
+
+(-part2 coordinates)
+;; => 16925
