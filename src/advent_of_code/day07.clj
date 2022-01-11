055d7559d093eeeb6818fe08e909537ab3ed75cc
Andrew Marquez <andrew.marquez@taxbit.com>
2022-01-10 17:56:04 -0700

    Solve day 7 part 2

--- src/advent_of_code/day07.clj
+++ src/advent_of_code/day07.clj
@@ -7,16 +7,24 @@
        (mapv #(Integer/parseInt %))))
 
 (defn distances
-  [x positions]
-  (->> (mapv #(Math/abs (- x %)) positions)
+  [x positions rate-fn]
+  (->> (map #(Math/abs (- x %)) positions)
+       (map rate-fn)
        (reduce +)))
 
-(defn -part1 [positions]
+(defn solve [positions rate-fn]
   (let [s (apply min positions)
         e (inc (apply max positions))]
-    (->> (into {} (for [x (range s e)] {x (distances x positions)}))
+    (->> (into {} (for [x (range s e)] {x (distances x positions rate-fn)}))
          (apply min-key val)
          val)))
 
-(-part1 input)
+;; Part 1
+(solve input identity)
 ;; => 340987
+
+;; 1 + 2 + 3 + ... + n
+(defn triangle [n] (reduce + (range 1 (inc n))))
+
+;; Part 2
+(solve input triangle)
