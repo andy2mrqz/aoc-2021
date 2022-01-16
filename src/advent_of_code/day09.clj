10db5c65c837a94e7f01f40789a1120fcd84649f
Andrew Marquez <andrew.marquez@taxbit.com>
2022-01-16 00:37:32 -0700

    Solve day 9 part 2

--- src/advent_of_code/day09.clj
+++ src/advent_of_code/day09.clj
@@ -16,14 +16,46 @@
         s (get-in m [(inc x) y] 9)
         e (get-in m [x (inc y)] 9)
         w (get-in m [x (dec y)] 9)]
-    (when (and (= it (min it n s e w)) (not= it n s e w)) (inc it))))
+    (when (and (= it (min it n s e w)) (not= it n s e w)) it)))
 
-(defn -part1 [hm]
+(defn low-points [hm]
   (->> (for [r (-> hm count range)
              c (-> (first hm) count range)]
-         (maybe-lowest hm r c))
-       (remove nil?)
-       (reduce +)))
+         [(maybe-lowest hm r c) r c])
+       (remove #(-> % first nil?))))
+
+(defn -part1 [hm] (reduce #(+ %1 (-> %2 first inc)) 0 (low-points hm)))
 
 (-part1 input)
 ;; => 550
+
+(defn valid? [hm s k]
+  (false? (or (contains? s k)
+              (when-let [val (get-in hm k)] (= val 9)))))
+
+(defn dfs [hm s [x y :as key]]
+  (let [visited (conj s key)
+        nkey [(dec x) y]
+        skey [(inc x) y]
+        ekey [x (inc y)]
+        wkey [x (dec y)]
+        nvis (if (valid? hm s nkey) (dfs hm visited nkey) visited)
+        svis (if (valid? hm s skey) (dfs hm nvis skey) nvis)
+        evis (if (valid? hm s ekey) (dfs hm svis ekey) svis)
+        wvis (if (valid? hm s wkey) (dfs hm evis wkey) evis)]
+    wvis))
+
+(defn basin-size [hm [v r c]]
+  ;; dfs of hm, keeping track of visited state
+  (->> (dfs hm #{} [r c])))
+
+(defn -part2 [hm]
+  (->> (low-points hm)
+       (mapv (partial basin-size hm))
+       (sort-by count)
+       (take-last 3)
+       (mapv count)
+       (apply *)))
+
+(-part2 input)
+;; => 1100682
