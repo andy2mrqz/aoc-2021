a3b4b3d6e8cc8e2fb6ebb61bf291c74597981757
Andrew Marquez <andrew.marquez@taxbit.com>
2022-01-17 15:02:32 -0700

    Solve day 10 part 2

--- src/advent_of_code/day10.clj
+++ src/advent_of_code/day10.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code.day10
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]))
 
 (def input
   (->> (slurp "resources/day10.txt")
@@ -26,12 +27,39 @@
       (reduced char))
     (conj stack char)))
 
-(defn -part1
-  [chunks]
-  (->> (mapv #(reduce find-corrupted '() %) chunks)
-       (filter char?)
+(defn corrupted [chunks] (mapv #(reduce find-corrupted '() %) chunks))
+
+(defn -part1 [chunks]
+  (->> (filter char? (corrupted chunks))
        (mapv score-table)
        (apply +)))
 
 (-part1 input)
 ;; => 166191
+
+(def score-table2
+  {\) 1
+   \] 2
+   \} 3
+   \> 4})
+
+(defn score [res completion]
+  (-> (* 5 res)
+      (+ (score-table2 completion))))
+
+(defn final-score [scores]
+  (scores (-> (count scores)
+              (quot 2))))
+
+(defn -part2 [chunks]
+  (->>
+   (filter list? (corrupted chunks))
+   (mapv #(mapv (set/map-invert pairs) %))
+   (mapv #(apply str %))
+   (mapv #(reduce score 0 %))
+   sort
+   vec
+   final-score))
+
+(-part2 input)
+;; => 1152088313
