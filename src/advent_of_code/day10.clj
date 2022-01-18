d65af7c4d8cc25902b34a20bb943ac4ac2fc960a
Andrew Marquez <andrew.marquez@taxbit.com>
2022-01-17 22:08:41 -0700

    Simplify

--- src/advent_of_code/day10.clj
+++ src/advent_of_code/day10.clj
@@ -1,23 +1,14 @@
 (ns advent-of-code.day10
-  (:require [clojure.string :as str]
-            [clojure.set :as set]))
+  (:require [clojure.string :as str]))
 
 (def input
   (->> (slurp "resources/day10.txt")
        (str/split-lines)
        (mapv seq)))
 
-(def pairs
-  {\) \(
-   \] \[
-   \} \{
-   \> \<})
-
-(def score-table
-  {\) 3
-   \] 57
-   \} 1197
-   \> 25137})
+(def pairs {\) \(, \] \[, \} \{, \> \<})
+(def score-table {\) 3, \] 57, \} 1197, \> 25137})
+(def score-table2 {\( 1, \[ 2, \{ 3, \< 4})
 
 (defn find-corrupted
   [stack char]
@@ -37,29 +28,17 @@
 (-part1 input)
 ;; => 166191
 
-(def score-table2
-  {\) 1
-   \] 2
-   \} 3
-   \> 4})
-
 (defn score [res completion]
-  (-> (* 5 res)
-      (+ (score-table2 completion))))
+  (+ (* res 5)
+     (score-table2 completion)))
 
 (defn final-score [scores]
-  (scores (-> (count scores)
-              (quot 2))))
+  (nth (sort scores) (/ (count scores) 2)))
 
 (defn -part2 [chunks]
-  (->>
-   (filter list? (corrupted chunks))
-   (mapv #(mapv (set/map-invert pairs) %))
-   (mapv #(apply str %))
-   (mapv #(reduce score 0 %))
-   sort
-   vec
-   final-score))
+  (->> (filter list? (corrupted chunks))
+       (mapv #(reduce score 0 %))
+       final-score))
 
 (-part2 input)
 ;; => 1152088313
