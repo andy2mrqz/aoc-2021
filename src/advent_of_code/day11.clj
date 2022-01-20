8774267c155a870eddfcc1b9a11f5c0f7fc82ec2
Andrew Marquez <andrew.marquez@taxbit.com>
2022-01-20 16:26:41 -0700

    Refactor day 11 using iterate

--- src/advent_of_code/day11.clj
+++ src/advent_of_code/day11.clj
@@ -27,42 +27,40 @@
   [coord]
   (mapv #(mapv + coord %) [[-1 -1] [-1 0] [-1 1] [0 -1] [0 1] [1 -1] [1 0] [1 1]]))
 
-(defn run-step [octos]
-  (let [;; step 1
-        octos1  (update-energy octos nil inc)
-        ;; step 2.1
-        octoloop (loop [os octos1]
-                   (let [mature (coords-test os #(<= 10 % 500))]
-                     (if (seq mature)
-                       (let [adj (mapcat adjacent mature)
-                             inc-mature (update-energy os mature (constantly 5000))
-                             next-os (update-energy inc-mature adj inc)]
-                         (recur next-os))
-                       os)))
-        octoloop-gt9 (coords-test octoloop #(> % 9))
-        flashes (count octoloop-gt9)
-        ;; step 3
-        octos3 (update-energy octoloop octoloop-gt9 (constantly 0))]
-    [octos3 flashes]))
+(defn run-step [octos coords]
+  (let [coords' (if (seq coords) coords (coords-test octos identity))
+        inced (update-energy octos coords' inc)]
+    (if-some [mature (seq (coords-test inced #(<= 10 % 500)))]
+      (let [adj (mapcat adjacent mature)
+            inced-mature (update-energy inced mature (constantly 5000))]
+        (recur inced-mature adj))
+      (let [flashed (coords-test inced #(> % 9))
+            final-octos (update-energy inced flashed (constantly 0))]
+        [final-octos nil (count flashed)]))))
+
+(defn octos-count [octos] (count (coords-test octos identity)))
+(defn iterations [initial-octos] (iterate (fn [[o c]] (run-step o c)) [initial-octos nil 0]))
 
 (defn -part1
   [octos steps]
-  (loop [octos octos, flashes 0, step 0]
-    (if (= step steps)
-      flashes
-      (let [[new-octos next-flashes] (run-step octos)]
-        (recur new-octos (+ flashes next-flashes) (inc step))))))
+  (->> (iterations octos)
+       (take (inc steps))
+       (mapv #(nth % 2))
+       (apply +)))
 
 (-part1 input 100)
 ;; => 1659
 
 (defn -part2
   [octos]
-  (loop [octos octos, flashes 0, step 0]
-    (if (= flashes (count (coords-test octos identity)))
-      step
-      (let [[new-octos next-flashes] (run-step octos)]
-        (recur new-octos next-flashes (inc step))))))
+  (->> (iterations octos)
+       (map #(nth % 2))
+       (keep-indexed #(when (= %2 (octos-count octos)) %1))
+       first))
 
 (-part2 input)
 ;; => 227
+
+;; PS:
+;; Ideas to refactor part1 and part2 using iterate come from abyala:
+;; https://github.com/abyala/advent-2021-clojure/blob/main/src/advent_2021_clojure/day11.clj
