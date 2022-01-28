62a727ed57bec4f6102e360e3a1dcea83bf381bb
Andrew Marquez <andrew.marquez@taxbit.com>
2022-01-28 10:26:30 -0700

    Solve without division or bigdec conversion

--- src/advent_of_code/day14.clj
+++ src/advent_of_code/day14.clj
@@ -6,36 +6,31 @@
        (str/split-lines)))
 
 (def template (seq (first input)))
-
 (def rules (->> (drop 2 input)
                 (map (comp (juxt (comp seq first) (comp first second))
                            #(re-seq #"\w+" %)))
                 (into {})))
 
-(defn safe+ [a b] (+ (or a 0) b))
-(defn update-ab-safe+ [m ka kb x] (-> (update m ka safe+ x)
-                                      (update kb safe+ x)))
+(defn update-safe+ [m k x] (update m k (fn [a b] (+ (or a 0) b)) x))
 
 (defn pair-insert [pair-freqs]
   (reduce (fn [pairs [pairA pairB :as pair]]
             (let [match (rules pair)]
-              (update-ab-safe+ pairs
-                               (list pairA match)
-                               (list match pairB) (pair-freqs pair)))) {} (keys pair-freqs)))
+              (-> (update-safe+ pairs (list pairA match) (pair-freqs pair))
+                  (update-safe+ (list match pairB) (pair-freqs pair))))) {} (keys pair-freqs)))
 
 (defn solve [steps]
   (let [pair-iterations (iterate pair-insert (frequencies (partition 2 1 template)))
-        pair-freqs (nth pair-iterations steps)
-        char-freqs (reduce (fn [fqs [[a b] count]]
-                             (update-ab-safe+ fqs a b count)) {} pair-freqs)
+        results (nth pair-iterations steps)
+        char-freqs (reduce (fn [fqs [[a] occurrences]] (update-safe+ fqs a occurrences)) {(last template) 1} results)
         most-common (val (apply max-key val char-freqs))
         least-common (val (apply min-key val char-freqs))]
-    (str (bigdec (Math/ceil (/ (- most-common least-common) 2))))))
+    (- most-common least-common)))
 
 ;; part 1
 (solve 10)
-;; => "2447.0"
+;; => 2447
 
 ;; part 2
 (solve 40)
-;; => "3018019237563"
+;; => 3018019237563
